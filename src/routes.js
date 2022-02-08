7b657c7bb7bd6edbcc99edc41771ea144acb6ebe
phaelslima <phaelslima@gmail.com>
2022-02-08 07:38:09 -0300

    style: rename files to .ejs and move to views folder

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => res.render('index'))
+router.get('/', (req, res) => res.render('home'))
+router.get('/room', (req, res) => res.render('room'))
+router.get('/create-pass', (req, res) => res.render('create-pass'))
 
 module.exports = router
