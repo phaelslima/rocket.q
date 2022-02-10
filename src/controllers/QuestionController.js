b143f13057f28e042086fc3e5d7ada9f94c8ea83
phaelslima <phaelslima@gmail.com>
2022-02-10 19:57:22 -0300

    feat: add index  and create methods to QuestionController

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -1,11 +1,49 @@
+const Database = require('../db/config')
+
 module.exports = {
-  index(req, res) {
+  async index(req, res) {
+    const db = await Database()
     const roomId = req.params.room
     const questionId = req.params.question
     const action = req.params.action
 
-    const pass = req.body.password
+    const password = req.body.password
 
-    console.log(roomId, questionId, action, pass)
+    const verifyRoom = await db.get(`SELECT * FROM rooms WHERE id = ${roomId}`)
+
+    if (verifyRoom.pass === password) {
+      if (action == 'check') {
+        await db.run(`UPDATE questions SET read = 1 WHERE id = ${questionId}`)
+      } else if (action == 'delete') {
+        await db.run(`DELETE FROM questions WHERE id = ${questionId}`)
+      }
+
+      await db.close()
+
+      res.redirect(`/room/${roomId}`)
+    } else {
+      res.render('wrong', { roomId })
+    }
+  },
+  async create(req, res) {
+    const db = await Database()
+    const roomId = req.params.room
+    const question = req.body.question
+
+    await db.run(`
+      INSERT INTO questions (
+        title,
+        read,
+        room_id
+      ) VALUES (
+        "${question}",
+        0,
+        ${parseInt(roomId)}
+      )
+    `)
+
+    await db.close()
+
+    res.redirect(`/room/${roomId}`)
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -8,14 +8,16 @@ router.get('/', (req, res) => res.render('index', { page: 'enter-room' }))
 router.get('/create-room', (req, res) =>
   res.render('index', { page: 'create-room' })
 )
+router.post('/create-room', RoomController.create)
 
 router.post('/enter-room/', RoomController.enter)
 router.get('/room/:room', RoomController.open)
 
-router.post('/room/:room/:question/:action', QuestionController.index)
-router.post('/create-room', RoomController.create)
 router.get('/not-found', (req, res) =>
   res.render('index', { page: 'not-found' })
 )
+
+router.post('/question/create/:room', QuestionController.create)
+router.post('/question/:room/:question/:action', QuestionController.index)
 
 module.exports = router
