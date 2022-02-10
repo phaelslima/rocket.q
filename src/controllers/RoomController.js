a3f0ec24d34e55540855639c5bbbe8077baa6e11
phaelslima <phaelslima@gmail.com>
2022-02-10 19:50:46 -0300

    feat: add create, enter and open methods to RoomController

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,7 +1,67 @@
+const Database = require('../db/config')
 module.exports = {
-  create(req, res) {
-    let roomId = 123456
+  async create(req, res) {
+    const db = await Database()
+    const pass = req.body.password
+    let roomId
+    let isRoom = true
+
+    while (isRoom) {
+      roomId = ''
+
+      for (let i = 0; i < 6; i++) {
+        roomId += Math.floor(Math.random() * 10).toString()
+      }
+
+      const rooms = await db.all(`SELECT id FROM rooms WHERE id = ${roomId}`)
+      isRoom = rooms.length == 0 ? false : true
+
+      if (!isRoom) {
+        await db.run(`
+          INSERT INTO rooms (
+            id,
+            pass
+          ) VALUES (
+            ${parseInt(roomId)},
+            "${pass}"
+          )
+        `)
+      }
+    }
+
+    await db.close()
 
     res.redirect(`/room/${roomId}`)
+  },
+  async open(req, res) {
+    const db = await Database()
+
+    const roomId = req.params.room
+
+    const questions = await db.all(
+      `SELECT * FROM questions WHERE room_id = ${roomId} ORDER BY read, id ASC`
+    )
+
+    await db.close()
+
+    res.render('room', { roomId, questions })
+  },
+  async enter(req, res) {
+    const db = await Database()
+    const roomId = req.body.roomId
+
+    if (roomId) {
+      const roomExistID = await db.get(
+        `SELECT * FROM rooms WHERE id = ${roomId}`
+      )
+
+      if (roomExistID) {
+        res.redirect(`/room/${roomId}`)
+      } else {
+        res.redirect(`not-found`)
+      }
+    } else {
+      res.redirect(`not-found`)
+    }
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,9 @@ router.get('/', (req, res) => res.render('index', { page: 'enter-room' }))
 router.get('/create-room', (req, res) =>
   res.render('index', { page: 'create-room' })
 )
-router.get('/room/:room', (req, res) => res.render('room'))
+
+router.post('/enter-room/', RoomController.enter)
+router.get('/room/:room', RoomController.open)
 
 router.post('/room/:room/:question/:action', QuestionController.index)
 router.post('/create-room', RoomController.create)
